f9edcf37426c74f4b9a5894884d203ce680fd0ca
David Perelroizen <david.perelroizen@gmail.com>
2022-09-16 05:49:31 +0200

    Implemented address model test

--- lettings/tests/test_models.py
+++ lettings/tests/test_models.py
@@ -1,2 +1,20 @@
 import pytest
 from django.urls import reverse, resolve
+from lettings.models import Address, Letting
+
+
+@pytest.mark.django_db
+def test_address_model_creation():
+    address_for_test = Address()
+    address_for_test.number = 53
+    address_for_test.street = 'rue de la crête'
+    address_for_test.city = 'Annecy'
+    address_for_test.state = 'HS'
+    address_for_test.zip_code = 74960
+    address_for_test.country_iso_code = 'FRA'
+    address_for_test.save()
+
+    expected_value = '53 rue de la crête'
+
+    assert str(address_for_test) == expected_value
+
