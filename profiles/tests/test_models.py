1bb90b75882995d5645eb0c7db95b658889cbc7f
David Perelroizen <david.perelroizen@gmail.com>
2022-09-16 05:42:51 +0200

    Implemented profile model test

---
+++ profiles/tests/test_models.py
@@ -0,0 +1,19 @@
+import pytest
+from django.urls import reverse, resolve
+from profiles.models import Profile
+from django.contrib.auth.models import User
+
+
+@pytest.mark.django_db
+def test_model_profile():
+    user_for_test = User()
+    user_for_test.username = 'test_user'
+    user_for_test.save()
+    profile_for_test = Profile()
+    profile_for_test.user = user_for_test
+    profile_for_test.favorite_city = 'dijon'
+    profile_for_test.save()
+
+    expected_value = 'test_user likes dijon'
+
+    assert str(profile_for_test)+' likes '+profile_for_test.favorite_city == expected_value
