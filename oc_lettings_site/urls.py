3c477f64417430cc4420aba95a623cdaf3fd14cf
David Perelroizen <david.perelroizen@gmail.com>
2022-09-16 06:25:19 +0200

    Modified the urls patterns so that it would include all the urls defined in the oc_lettings_site_base.urls

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', include('oc_lettings_site_base.urls')),
     path('admin/', admin.site.urls),
     path('profiles/', include('profiles.urls')),
     path('lettings/', include('lettings.urls')),
