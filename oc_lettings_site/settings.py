8b6328399ffe19473947781c55523ed40f4bf70a
David Perelroizen <david.perelroizen@gmail.com>
2022-09-23 19:36:02 +0200

    Sentry set-up

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,5 +1,7 @@
 import os
 import django_heroku
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -137,5 +139,28 @@ TEST_OUTPUT_DIR = 'test-results'
 
 
 WHITENOISE_USE_FINDERS = True
+
+
+sentry_sdk.init(
+    dsn="https://76ebbbaf635b42a0ba38c165944cbcef@o1423942.ingest.sentry.io/6771686",
+    max_breadcrumbs=50,
+    debug=True,
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production,
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+
+    # By default the SDK will try to use the SENTRY_RELEASE
+    # environment variable, or infer a git commit
+    # SHA as release, however you may want to set
+    # something more human-readable.
+    # release="myapp@1.0.0",
+)
 
 django_heroku.settings(locals())
