78a8e8f999f867f5efc94f76dce311662dc6f06c
David Perelroizen <david.perelroizen@gmail.com>
2022-09-28 16:29:19 +0200

    Modified test_views for oc_lettings_site_base to integrate h1 tag check

--- oc_lettings_site_base/tests/test_views.py
+++ oc_lettings_site_base/tests/test_views.py
@@ -3,6 +3,7 @@ import pytest
 from django.urls import reverse
 from django.test import Client
 from pytest_django.asserts import assertTemplateUsed
+from bs4 import BeautifulSoup
 
 
 @pytest.mark.django_db
@@ -12,5 +13,11 @@ def test_profile_index_view():
 
     response = client.get(path)
 
+    soup = BeautifulSoup(response.content, features="html.parser")
+    soup_content = soup.find_all("h1")
+
+    assertion_check = 'Welcome to Holiday Homes'
+
     assert response.status_code == 200
     assertTemplateUsed(response, 'oc_lettings_site_base/index.html')
+    assert assertion_check == soup_content[0].get_text()
