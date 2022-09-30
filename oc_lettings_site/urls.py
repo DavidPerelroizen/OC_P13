5f7e4da6f1a67c222d22ecb84837387b2a0df922
David Perelroizen <david.perelroizen@gmail.com>
2022-09-30 21:15:22 +0200

    Added a trigger error view and path

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
     path('', include('oc_lettings_site_base.urls', namespace='oc-lettings-site-base')),
     path('admin/', admin.site.urls),
     path('profiles/', include('profiles.urls', namespace='profiles')),
     path('lettings/', include('lettings.urls', namespace='lettings')),
+    path('sentry-debug/', trigger_error),
 ]
